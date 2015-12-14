98a7586353ae10e2be2810f468e023a0266cb461
Kyle Asaff <kyleasaff@pixleeteam.com>
2015-12-13 20:28:30 -0800

    Zero Cater support

--- bin/bot.js
+++ bin/bot.js
@@ -1,7 +1,10 @@
 var RamsayBot = require('../lib/ramsaybot');
 
-var token = process.env.BOT_API_KEY;
-var name = process.env.BOT_NAME;
+//var token = process.env.BOT_API_KEY;
+//var name = process.env.BOT_NAME;
+
+var token = 'xoxb-15179089074-tngobgb21Lyqw5s0t0BSOhi4'
+var name = 'ramsay_bot'
 
 var ramsaybot = new RamsayBot({
     token: token,
